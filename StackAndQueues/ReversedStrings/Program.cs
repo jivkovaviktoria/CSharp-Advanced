8354e44bb34e79bb49f72fa91cdae6fee1297ce5
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-08-08 17:21:03 +0300

    Solved "Reversed strings" from Stacks and queues - lab

---
+++ StackAndQueues/ReversedStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReversedStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversedInput = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+                reversedInput.Push(input[i]);
+
+            int count = reversedInput.Count;
+
+            for (int i = 0; i < count; i++)
+                Console.Write(reversedInput.Pop());
+        }
+    }
+}
