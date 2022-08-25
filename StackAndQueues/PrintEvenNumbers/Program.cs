da3b6c99a77faa3c76faec0bbb715a318c0c89d3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-08-25 17:58:18 +0300

    Solved "Print even numbers" from Stacks and queues - lab

---
+++ StackAndQueues/PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    numbers.Enqueue(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
