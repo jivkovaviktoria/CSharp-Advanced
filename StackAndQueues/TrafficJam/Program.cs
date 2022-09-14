774f8201b9b7b400d2f48a1969f496be2af26451
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-14 13:45:04 +0300

    Solved "Traffic Jam" from Stacks and queues - lab

---
+++ StackAndQueues/TrafficJam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            var cars = new Queue<string>();
+
+            int passedCarsCount = 0;
+
+            while (command!="end")
+            {
+                if (command != "green")
+                    cars.Enqueue(command);
+                else if(command == "green")
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (cars.Count > 0)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            passedCarsCount++;
+                        }
+                        else
+                            break;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCarsCount} cars passed the crossroads.");
+        }
+    }
+}
