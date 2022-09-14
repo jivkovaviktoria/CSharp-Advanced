a3b274480bbe79dd634bd74121f793ddb16f4e2e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-14 15:04:47 +0300

    Solved "Basic stack operations" from Stacks and queues - exercise

---
+++ StackAndQueues/BasicStackOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] countOfOperations = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int numToFind = countOfOperations[2];
+            int countOfNumsToPop = countOfOperations[1];
+
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var stackOfNums = new Stack<int>(nums);
+
+                for (int i = 0; i < countOfNumsToPop; i++)
+                    stackOfNums.Pop();
+
+            if (stackOfNums.Contains(numToFind))
+                Console.WriteLine("true");
+            else if(stackOfNums.Count == 0)
+                Console.WriteLine(0);
+            else
+                Console.WriteLine(stackOfNums.Min());
+           
+
+        }
+    }
+}
