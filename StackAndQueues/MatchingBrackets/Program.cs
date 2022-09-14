2441a700ff3ad60921fb611fd86fb7a1bd97aa64
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-14 14:36:16 +0300

    Solved "Matching brackets" from Stacks and queues - lab

---
+++ StackAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                    stack.Push(i);
+                else if (input[i] == ')')
+                    Console.WriteLine(input.Substring(stack.Peek(), i - stack.Pop() + 1));
+            }
+        }
+    }
+}
