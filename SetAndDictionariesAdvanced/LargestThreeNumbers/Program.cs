3dabb5cebda0c04a9109b5d1acb511bc21bf3cdf
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-21 11:02:03 +0300

    Solved "Largest three numbers" from Sets and dictionaries - lab

---
+++ SetAndDictionariesAdvanced/LargestThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToList();
+            for (int x = 0; x < integers.Count; x++)
+            {
+                if (x == 3) break;
+                Console.Write($"{integers[x]} ");
+            }
+
+        }
+    }
+}
