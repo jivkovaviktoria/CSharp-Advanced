af2f66bf0fd0ade5c1deb84a216f6ffbab5a1f18
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-23 11:02:03 +0300

    Solved "Unique usernames" from Sets and dictionaries - exercise

---
+++ SetAndDictionariesAdvanced/UniqueUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var username = Console.ReadLine();
+                uniqueUsernames.Add(username);
+            }
+
+            foreach (var user in uniqueUsernames) Console.WriteLine(user);
+        }
+    }
+}
---
+++ SetAndDictionariesAdvanced/Wardrobe/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> clothesByColor = new Dictionary<string, Dictionary<string, int>>();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] data = Console.ReadLine().Split(" -> ");
+                var color = data[0];
+
+                string[] clothes = data[1].Split(",");
+
+                if (!clothesByColor.ContainsKey(color))
+                    clothesByColor.Add(color, new Dictionary<string, int>());
+
+                foreach (var item in clothes)
+                {
+                    if (!clothesByColor[color].ContainsKey(item))
+                        clothesByColor[color].Add(item, 0);
+
+                    clothesByColor[color][item]++;
+                }
+            }
+
+            string[] tokens = Console.ReadLine().Split();
+            string colorOfItem = tokens[0];
+            string itemToFind = tokens[1];
+
+            foreach (var item in clothesByColor)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+                foreach (var cloth in item.Value)
+                {
+                    if (colorOfItem == item.Key && itemToFind == cloth.Key)
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    else
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+
+                }
+            }
+        }
+    }
+}
