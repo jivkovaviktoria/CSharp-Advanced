3f1a27de828d21f01f3812c95a9971fd23252cfc
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-23 11:04:25 +0300

    Solved "Periodic table" from Sets and dictionaries - exercise

---
+++ SetAndDictionariesAdvanced/PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> setOfElements = new HashSet<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                foreach (var el in elements) setOfElements.Add(el);
+            }
+
+            Console.WriteLine(string.Join(" ", setOfElements.OrderBy(x => x).ToArray()));
+        }
+    }
+}
