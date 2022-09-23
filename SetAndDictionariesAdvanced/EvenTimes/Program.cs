a3e9c383f049d1aa7bf98dc8046107707f2f1a28
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-23 11:05:10 +0300

    Solved "Even times" from Sets and dictionaries - exercise

---
+++ SetAndDictionariesAdvanced/EvenTimes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbersCount = new Dictionary<int, int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < count; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                if (!numbersCount.ContainsKey(num))
+                    numbersCount.Add(num, 1);
+                else
+                    numbersCount[num]++;
+            }
+
+            foreach(var num in numbersCount)
+            {
+                if(num.Value%2==0)
+                    Console.WriteLine(num.Key);
+            }
+        }
+    }
+}
