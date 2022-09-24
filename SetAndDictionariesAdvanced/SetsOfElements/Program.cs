e18249d7f39724eae0d3ca4aecd7d68f0a90aa8b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-24 11:37:43 +0300

    Solved "Sets of elements" from Sets and dictionaries - exercise

---
+++ SetAndDictionariesAdvanced/SetsOfElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    internal class Program
+    {
+        public static object HashSet { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstSetLength = lengths[0];
+            var secondSetLength = lengths[1];
+
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            for (int i = 0; i < firstSetLength; i++) first.Add(int.Parse(Console.ReadLine()));
+
+            for (int i = 0; i < secondSetLength; i++) second.Add(int.Parse(Console.ReadLine()));
+
+            first.IntersectWith(second);
+            Console.WriteLine(String.Join(" ", first));
+        }
+    }
+}
