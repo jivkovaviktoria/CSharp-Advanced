2c46f31dcb35ed0b73ca20cbcd423aa80518f775
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-25 23:50:06 +0300

    Solved "Sum numbers" from Functional programming - lab

---
+++ FunctionalProgramming/SumNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.CountNumbers());
+            Console.WriteLine(numbers.SumNumbers());
+        }
+    }
+    public static class Methods
+    {
+        public static int SumNumbers(this int[] collection)
+        {
+            var result = 0;
+
+            foreach (var item in collection)
+                result += item;
+
+            return result;
+        }
+
+        public static int CountNumbers(this int[] collection)
+        {
+            var result = 0;
+
+            foreach (var item in collection)
+                result++;
+
+            return result;
+        }
+    }
+}
