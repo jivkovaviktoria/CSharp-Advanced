04bda5ca98e63f0a88b77f2b0f89b57ce55fe536
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-25 23:46:33 +0300

    Solved "Sort even numbers" from Functional programming - lab

---
+++ FunctionalProgramming/SortEvenNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SortEvenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            x = x.SortEven().ToArray();
+
+            Console.WriteLine(string.Join(", ", x));
+        }
+    }
+
+    public static class Methods
+    {
+        public static List<int> SortEven(this int[] collection)
+        {
+            var result = new List<int>();
+
+            foreach(var item in collection)
+            {
+                if (item % 2 == 0)
+                    result.Add(item);
+            }
+
+            result.Sort();
+            return result;
+        }
+    }
+}
