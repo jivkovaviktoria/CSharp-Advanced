885a420277fc58ab7fa43afa8446f73d2d25bcaa
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-25 15:41:07 +0300

    Solved "Copy binary file" from Streams files and directories

---
+++ StreamsFilesAndDirectories/CopyBinaryFile/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            byte[] buffer;
+            using (var reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                buffer = new byte[reader.Length];
+                reader.Read(buffer, 0, buffer.Length);
+            }
+            using (var writer = new FileStream(outputFilePath, FileMode.Create))
+            {
+                writer.Write(buffer);
+            }
+        }
+    }
+}
