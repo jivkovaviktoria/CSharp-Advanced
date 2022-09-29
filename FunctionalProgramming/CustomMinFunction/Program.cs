6663ba240497eb1db8ec9f6e8746c09dae1b9c90
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-29 16:59:04 +0300

    Solved "Custom min function" from Functional programming - exercise /using Func/

---
+++ FunctionalProgramming/CustomMinFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func<List<int>, int> GetMinNumber = nums => nums.Min();
+
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Console.WriteLine(GetMinNumber(numbers));
+        }
+    }
+}
