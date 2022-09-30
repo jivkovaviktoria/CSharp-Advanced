c9e9701da2052bab35848d5ea4529b2648339265
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-30 11:10:37 +0300

    Solved "Action point" from Functional programming - exercise

---
+++ FunctionalProgramming/ActionPoint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = Console.ReadLine().Split().ToList();
+
+            Action<List<string>> Print = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+            Print(collection);
+        }
+    }
+}
