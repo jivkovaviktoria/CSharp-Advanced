bb046fafb9bb0ecedf09176e8225ebbedfd4b735
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-09-30 11:14:43 +0300

    Solved "Knights of honor" from Functional programming - exercise

---
+++ FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split().ToList();
+            names.ForEach(x => Console.WriteLine($"Sir {x}"));
+        }
+    }
+}
