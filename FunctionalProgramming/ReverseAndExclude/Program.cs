3278b7a6be98e5a4b7fe625a855ff50dc18d1a3f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-02 09:48:01 +0300

    Solved "Reverse and exclude" from Functional programming - exercise

---
+++ FunctionalProgramming/ReverseAndExclude/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int divisor = int.Parse(Console.ReadLine());
+            numbers.Reverse();
+
+            bool IsDivisor(int x)
+            {
+                if (x % divisor != 0) return true;
+                else return false;
+            }
+
+            var predicate = new Predicate<int>(IsDivisor);
+            var result = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                var isDivisor = predicate(num);
+                if (isDivisor) result.Add(num);
+            }
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+       
+    }
+}
