998ede8b7bf4379d3b831f6fcaa3fb25d026cd74
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-02 10:04:08 +0300

    Solved "Predicate for names" from Functional programming - exercise

---
+++ FunctionalProgramming/PredicateForNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Predicate<string> isValid = x => x.Length <= maxLength;
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(x => isValid(x))));
+        }
+    }
+}
