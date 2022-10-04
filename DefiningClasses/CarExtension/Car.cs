54bed4f964883595d3685154b01d96032cc68a64
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-04 19:33:28 +0300

    Solved "Car extension" from Defining classes - lab

---
+++ DefiningClasses/CarExtension/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - (distance * this.FuelConsumption) > 0)
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            else
+                Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.Append($"Fuel: {this.FuelQuantity:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
