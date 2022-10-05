a6cc3c140b492da11f831390eef8ac8cf8f8afdc
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-05 18:40:44 +0300

    Solved "Opinion poll" from Defining classes - exercise

---
+++ DefiningClasses/OpinionPoll/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+        }
+
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
---
+++ DefiningClasses/OpinionPoll/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+
+                var person = new Person(name, age);
+                people.Add(person);
+            }
+
+            foreach (var person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            
+        }
+    }
+}
