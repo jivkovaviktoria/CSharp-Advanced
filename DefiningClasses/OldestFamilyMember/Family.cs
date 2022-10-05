9f58eb471f865b9f025165ee3a2a52e7f37e5b76
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-05 18:35:03 +0300

    Solved "Oldest family member" from Defining classes - exercise

---
+++ DefiningClasses/OldestFamilyMember/Family.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member) => this.People.Add(member);
+        public Person GetOldestMember() => this.People.OrderByDescending(x => x.Age).FirstOrDefault();
+        
+    }
+}
---
+++ DefiningClasses/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                var name = tokens[0];
+                var age = int.Parse(tokens[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+            Console.WriteLine(oldestMember.Name + " " + oldestMember.Age);
+        }
+    }
+}
