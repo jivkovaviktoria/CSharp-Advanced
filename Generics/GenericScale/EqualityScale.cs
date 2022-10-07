2f14e10d4a515ee5e075cbfbf26d7327db2d12cf
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-07 23:08:48 +0300

    Solved "Equality scale" from Generics - lab

---
+++ Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    internal class EqualityScale<T>
+        where T : struct
+    {
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            if (this.Left.Equals(this.Right)) return true;
+            else return false;
+        }
+    }
+}
