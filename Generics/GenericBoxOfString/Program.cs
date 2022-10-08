f084f65ac121f58273ad2b25d8b19b02265da2bd
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-08 16:22:36 +0300

    Solved "Generic box of strings" from Generics - exercise

---
+++ Generics/GenericBoxOfString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Box<string> strings = new Box<string>();
+                strings.value = Console.ReadLine();
+
+                Console.WriteLine(strings.ToString());
+            }
+        }
+    }
+}
