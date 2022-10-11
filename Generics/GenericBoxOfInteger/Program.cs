fcd7416db353423f8565a8312ffb4c037b5de68b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-11 21:12:54 +0300

    Solved "Generic box of integers" from Generics - exercise

---
+++ Generics/GenericBoxOfInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> integers = new Box<int>();
+                integers.value = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(integers.ToString());
+            }
+        }
+    }
+}
