82c0176b04c37d67588fd373c24fb6e0f5b756f1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-11 21:25:43 +0300

    Solved "Generic swap method strings" from Generics - exercise

---
+++ Generics/GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> inputs = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                inputs.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Swap(inputs, indexes);
+
+            foreach(var item in inputs)
+                Console.WriteLine($"{item.GetType()}: {item}");
+        }
+
+        public static void Swap<T>(List<T> collection, int[] indexes)
+        {
+            var index1 = indexes[0];
+            var index2 = indexes[1];
+
+            (collection[index1], collection[index2]) = (collection[index2], collection[index1]);
+        }
+    }
+
+    
+}
