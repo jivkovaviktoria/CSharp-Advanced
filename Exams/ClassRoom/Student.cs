c774813ac7f66209077d547f592b7619fac59d0f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-12 11:35:08 +0300

    Solved "Classroom " from C# Advanced - exams

---
+++ Exams/ClassRoom/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassroomProject
+{
+    internal class Student
+    {
+        public Student(string fn, string ls, string subject)
+        {
+            FirstName = fn;
+            LastName = ls;
+            Subject = subject;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+
+        public override string ToString()
+        {
+            return $"Student: First Name = {FirstName}, Last Name = {LastName}, Subject = {Subject}";
+        }
+    }
+}
