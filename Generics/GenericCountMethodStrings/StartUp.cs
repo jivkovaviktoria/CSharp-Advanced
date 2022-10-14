533d55b6b12411fcd2d4be3abb3c1e0f3e3c4977
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-14 10:32:37 +0300

    Solved "Generic count method string" from Generics - exercise

---
+++ Generics/GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < n; i++) strings.Add(Console.ReadLine());
+
+            string element = Console.ReadLine();
+            Console.WriteLine(GetCount(strings, element));
+        }
+
+        public static int GetCount<T>(List<T> collection, T element) where T : IComparable<T>
+        {
+            int count = 0;
+            foreach(var x in collection)
+            {
+                if (x.CompareTo(element) > 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
