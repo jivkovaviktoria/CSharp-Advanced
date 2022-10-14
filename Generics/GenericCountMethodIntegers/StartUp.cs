b7304a9dab69efcdd0f510f16486736a6a2fc913
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-10-14 10:40:23 +0300

    Solved "Generic count method integer" from Generics - exercise

---
+++ Generics/GenericCountMethodIntegers/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodIntegers
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<double> doubles = new List<double>();
+
+            for (int i = 0; i < n; i++) doubles.Add(double.Parse(Console.ReadLine()));
+
+            double element = double.Parse(Console.ReadLine());
+            Console.WriteLine(GetCount(doubles, element));
+        }
+
+        public static int GetCount<T>(List<T> collection, T element) where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (var x in collection)
+            {
+                if (x.CompareTo(element) > 0)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
